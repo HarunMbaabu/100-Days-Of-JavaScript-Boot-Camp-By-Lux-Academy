b2be7166ff0e9df7b56868836eb8fdd48baf29bf
Lux Tech Academy <51136314+HarunHM@users.noreply.github.com>
2020-07-20 14:12:29 +0300

    create trim.js
    Add trim.js to demostrate the use of  trim method in javascript

---
+++ strings/trim.js
@@ -0,0 +1,5 @@
+/* remove all leading and trailing whitespace characters of a string, you use the trim() method. */
+
+let rawString = ' Hi  ';
+let strippedString = rawString.trim();
+console.log(strippedString); // "Hi"

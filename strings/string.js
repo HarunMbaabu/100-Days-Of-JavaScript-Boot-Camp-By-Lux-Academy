95dc991711d31ca6df40dba524079e1259acfe4f
Lux Tech Academy <51136314+HarunHM@users.noreply.github.com>
2020-07-20 13:58:37 +0300

    Updated string.js
    Added a file string.js

---
+++ strings/string.js
@@ -0,0 +1,11 @@
+let str = new String('JavaScript String Type');
+
+//To get Data from string 
+console.log(str); /*JavaScript String Type */
+
+//to get the length of the string 
+
+console.log(str.length); /*22 */
+
+//to access an individual character 
+console.log(str[0]); /* J */

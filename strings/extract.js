4191ce3056b4c2823aa2aa66aef21e6c85344814
Lux Tech Academy <51136314+HarunHM@users.noreply.github.com>
2020-07-20 14:05:14 +0300

    created extract.js
    added extract.js to demostrate the use of substr() method in JavaScript.

---
+++ strings/extract.js
@@ -0,0 +1,6 @@
+/* extract a substring from a string, you use the substr() method */
+
+let str = "JavaScript String";
+
+console.log(str.substr(0, 10)); /* "JavaScript" */
+console.log(str.substr(11,6)); /* "String" */

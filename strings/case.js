7914e7c1b84cf075ad38e8b3968f964f108b01db
Lux Tech Academy <51136314+HarunHM@users.noreply.github.com>
2020-07-20 14:13:56 +0300

    create case.js
    Add case.js to demonstrate the change cases of a string, you use toLowerCase() and toUpperCase() methods:

---
+++ strings/case.js
@@ -0,0 +1,5 @@
+ change cases of a string, you use toLowerCase() and toUpperCase() methods:
+
+let greeting = 'Hello';
+console.log(greeting.toLowerCase()); // 'hello'
+console.log(greeting.toUpperCase()); // 'HELLO';

05ffff36fe4fa2b54ba66399358c7572815eae8f
Lux Tech Academy <51136314+HarunHM@users.noreply.github.com>
2020-07-20 14:02:20 +0300

    Created Concatenate.js
    Added Concatenate.js to demostrate the concat() method

---
+++ strings/Concatenate.js
@@ -0,0 +1,5 @@
+/*  concatenate two or more strings you use the concat() method */
+let firstName = 'Lux';
+let fullName = firstName.concat(' ','Academy');
+console.log(fullName); /* "Lux Academy" */
+console.log(firstName); /* "Lux" */

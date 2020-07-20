40aaff79cb8ff003c2d746bdb327dd27dcef690a
Harun Mbaabu Mwenda <mbaabuharun8@gmail.com>
2020-07-20 09:21:26 +0300

    first commit on the initial snippets

---
+++ modal_project1/static/index.js
@@ -0,0 +1,29 @@
+
+let modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+let btn = document.getElementById("open");
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+
+
